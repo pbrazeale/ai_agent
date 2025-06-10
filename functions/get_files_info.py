4ed5594eb835548048cd133ea5f148290792ed31
pbrazeale <pbrazeale@gmail.com>
2025-06-10 09:14:19 -0500

    0.11.1

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,17 @@
+import os
+
+
 def get_files_info(working_directory, directory=None):
+    
     if directory not in working_directory:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    
+    if directory:
+        pass
+        '''
+        - README.md: file_size=1032 bytes, is_dir=False
+        - src: file_size=128 bytes, is_dir=True
+        - package.json: file_size=1234 bytes, is_dir=False
+        '''
+    else:
+        return f'Error: "{directory}" is not a directory'
