2925055bf65485ca98abab428add07be60070c39
pbrazeale <pbrazeale@gmail.com>
2025-06-10 09:22:55 -0500

    0.12.1

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,17 +1,18 @@
 import os
 
-
 def get_files_info(working_directory, directory=None):
-    
-    if directory not in working_directory:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    abs_path = os.path.abspath(directory)
 
-    if directory:
+    if abs_path.startswith(working_directory) == False:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if os.path.isdir(directory) == False:
+        return f'Error: "{directory}" is not a directory'
+
+    if os.path.isdir(directory):
         pass
         '''
         - README.md: file_size=1032 bytes, is_dir=False
         - src: file_size=128 bytes, is_dir=True
         - package.json: file_size=1234 bytes, is_dir=False
         '''
-    else:
-        return f'Error: "{directory}" is not a directory'
