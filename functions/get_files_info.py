63157a3ae5cbad4625e9b8d4c9a625ad5c570727
pbrazeale <pbrazeale@gmail.com>
2025-06-17 10:49:20 -0500

    0.12.8

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,7 +4,9 @@ def get_files_info(working_directory, directory=None):
     abs_path = os.path.abspath(directory)
     print(abs_path)
 
-    if abs_path.startswith(working_directory) == False:
+    root_dir = os.listdir()
+
+    if working_directory not in root_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
     if os.path.isdir(directory) == False:
