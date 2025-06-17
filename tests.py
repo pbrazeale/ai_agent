f0b7400fadb6fceaaaaa316c28668ae67b9d85e5
pbrazeale <pbrazeale@gmail.com>
2025-06-17 13:35:07 -0500

    0.16.0

--- tests.py
+++ tests.py
@@ -1,23 +1,11 @@
 # import unittest
-from functions.get_files_info import get_files_info
+from functions.get_files_content import get_file_content 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for lorem.txt")
     print(result)
     print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
 
 
 if __name__ == "__main__":
