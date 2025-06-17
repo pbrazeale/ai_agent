85402b03a3be69528ded118c56ad77f78394e2a0
pbrazeale <pbrazeale@gmail.com>
2025-06-17 14:11:43 -0500

    0.17.2

--- tests.py
+++ tests.py
@@ -1,27 +1,21 @@
 # import unittest
-from functions.get_files_content import get_file_content 
+from functions.write_files import write_file 
 
 def test():
-    # result = get_file_content("calculator", "lorem.txt")
-    # print("Result for lorem.txt")
-    # print(result)
-    # print("")
-
-    result = get_file_content("calculator", "main.py")
-    print("Result for main.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for lorem.txt")
     print(result)
     print("")
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for pkg/morelorem.txt")
     print(result)
     print("")
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for /bin/cat")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for /tmp/temp.txt")
     print(result)
     print("")
-
 
 if __name__ == "__main__":
     test()
