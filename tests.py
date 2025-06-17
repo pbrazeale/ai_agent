e8a6716c843bad7182aa7184ce215abb6887c5e8
pbrazeale <pbrazeale@gmail.com>
2025-06-17 13:36:09 -0500

    0.16.1

--- tests.py
+++ tests.py
@@ -7,6 +7,21 @@ def test():
     print(result)
     print("")
 
+    # result = get_file_content("calculator", "main.py")
+    # print("Result for main.py")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print("Result for pkg/calculator.py")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "/bin/cat")
+    # print("Result for /bin/cat")
+    # print(result)
+    # print("")
+
 
 if __name__ == "__main__":
     test()
