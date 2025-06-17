caa3f325fff41d239d56846cf8d4d59ac1e2d166
pbrazeale <pbrazeale@gmail.com>
2025-06-17 10:40:34 -0500

    0.12.6

--- tests.py
+++ tests.py
@@ -4,22 +4,22 @@ from functions.get_files_info import get_files_info
 # Testing get_files_info 
 class TestCalculator(unittest.TestCase):
     def test_calculator_current(self):
-        result = self.get_files_info("calculator", ".")
+        result = get_files_info("calculator", ".")
         print(result)
         # self.assertEqual(result, "")
 
     def test_calculator_pkg(self):
-        result = self.get_files_info("calculator", "pkg")
+        result = get_files_info("calculator", "pkg")
         print(result)
         # self.assertEqual(result, "")
 
     def test_calculator_bin(self):
-        result = self.get_files_info("calculator", "/bin")
+        result = get_files_info("calculator", "/bin")
         print(result)
         # self.assertEqual(result, "")
 
     def test_calculator_parent(self):
-        result = self.get_files_info("calculator", "../")
+        result = get_files_info("calculator", "../")
         print(result)
         # self.assertEqual(result, "")
 
