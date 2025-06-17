7400e7cd41d616c8de5863e1a9509561180c6115
pbrazeale <pbrazeale@gmail.com>
2025-06-17 11:22:05 -0500

    0.12.15

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,33 +2,32 @@ import os
 
 def get_files_info(working_directory, directory=None):
     root_dir = os.listdir(working_directory)
-    # print(root_dir)
-    working_dir_path = f"{os.path.abspath(working_directory)}"
-    print(working_dir_path)
-    target_path = f"{working_dir_path}/{directory}"
+    working_dir_path = os.path.abspath(working_directory)
+    target_path = f"{working_dir_path}/{directory}/"
     print(target_path)
 
     if directory not in root_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
     
-    if os.path.isdir(directory) == False:
+    if os.path.isdir(target_path) == False:
         return f'Error: "{directory}" is not a directory\n'
-
-    if os.path.isdir(target_path):
+    else:
+        print(os.listdir(target_path))
         try:
             dir_contents = os.listdir(target_path)
         except Exception as e:
             return f"Error: {str(e)}\n"
     
-        dir_formated_string = f""
+        dir_contents_formated = f""
     
         try:
             for item in dir_contents:
-                dir_formated_string + f"- {item}: file_size={os.path.getsize(item)}, is_dir={os.path.isdir(item)}\n"
+                dir_contents_formated += f"- {item}: file_size={os.path.getsize(f"{target_path}/{item}")}, is_dir={os.path.isdir(f"{target_path}/{item}")}\n"
         except Exception as e:
             return f"Error: {str(e)}\n"
-    
-        return dir_formated_string
+        
+        # print(dir_contents_formated)
+        return dir_contents_formated
         '''
         - README.md: file_size=1032 bytes, is_dir=False
         - src: file_size=128 bytes, is_dir=True
--- tests.py
+++ tests.py
@@ -3,10 +3,10 @@ from functions.get_files_info import get_files_info
 
 # Testing get_files_info 
 class TestCalculator(unittest.TestCase):
-    def test_calculator_current(self):
-        result = get_files_info("calculator", ".")
-        print(result)
-        # self.assertEqual(result, "")
+    # def test_calculator_current(self):
+    #     result = get_files_info("calculator", ".")
+    #     print(result)
+    #     # self.assertEqual(result, "")
 
     def test_calculator_pkg(self):
         result = get_files_info("calculator", "pkg")
