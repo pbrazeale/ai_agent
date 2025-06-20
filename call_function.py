4dce4ecac45fae9b9f065553b55d5aa2fd018e21
pbrazeale <pbrazeale@gmail.com>
2025-06-20 13:52:29 -0500

    0.24.0

--- call_function.py
+++ call_function.py
@@ -14,3 +14,12 @@ available_functions = types.Tool(
     ]
 )
 
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+
+
+
