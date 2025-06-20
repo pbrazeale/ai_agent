99c69d4d762d6f8524bc311edd17f6eaa3d51b51
pbrazeale <pbrazeale@gmail.com>
2025-06-20 13:28:15 -0500

    0.21.4

--- functions/run_python.py
+++ functions/run_python.py
@@ -29,3 +29,16 @@ def run_python_file(working_directory, file_path):
     except Exception as e:
         return f"Error: executing Python file: {e}"
     
+schema_run_python_file = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
