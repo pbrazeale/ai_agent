07c8380a45c07f4a27db656fdf8cff30b684c07a
pbrazeale <pbrazeale@gmail.com>
2025-06-20 13:35:30 -0500

    0.22.1

--- call_function.py
+++ call_function.py
@@ -1,7 +1,7 @@
 from google.genai import types
 
 from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
+from functions.get_files_content import schema_get_file_content
 from functions.run_python import schema_run_python_file
 from functions.write_files import schema_write_file
 
