0b40adb39507f5d128b0c0fff567183a696c6752
pbrazeale <pbrazeale@gmail.com>
2025-06-20 13:04:28 -0500

    0.19.2

--- main.py
+++ main.py
@@ -71,7 +71,8 @@ response = client.models.generate_content(
 )
 
 if response.function_calls:
-    print(f"Calling function: {response.function_call_part.name}({response.function_call_part.args})")
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {response.function_call_part.name}({response.function_call_part.args})")
 else:
     print(response.text)
 
