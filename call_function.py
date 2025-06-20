13004a60efac38ea94fd570ba8ed56d4e4d3005d
pbrazeale <pbrazeale@gmail.com>
2025-06-20 13:25:44 -0500

    0.21.1

--- call_function.py
+++ call_function.py
@@ -5,5 +5,9 @@ from functions.get_files_info import schema_get_files_info
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 )
+
