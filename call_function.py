3b246d0096f08a6822e28ccfa4caf272fc0ceaf9
pbrazeale <pbrazeale@gmail.com>
2025-06-20 15:18:21 -0500

    0.24.0

--- call_function.py
+++ call_function.py
@@ -19,7 +19,27 @@ def call_function(function_call_part, verbose=False):
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(f" - Calling function: {function_call_part.name}")
-    
+
+    try:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    except:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
 
 
 
